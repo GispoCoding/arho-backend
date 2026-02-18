1d81c8e300bcb42f755ecdff4b9838e1716bd5de
Lauri Kajan <lauri.kajan@gispo.fi>
2026-02-18 16:14:00 +0200

    Use the latest amazon linux image

--- infra/bastion.tf
+++ infra/bastion.tf
@@ -1,9 +1,13 @@
 # We don't want to create the key in terraform. Otherwise the private key(s) would be saved in terraform state.
 # Let's save the public key(s) here as ec2 instance user data.
 
+data "aws_ssm_parameter" "amazon_linux" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-arm64"
+}
+
 # Just the smallest arm instance available, for routing traffic to postgres
 resource "aws_instance" "bastion-ec2-instance" {
-  ami = "ami-0bf463e49ccd368ed" # Amazon Linux 2023
+  ami = data.aws_ssm_parameter.amazon_linux.value
   instance_type = "t4g.nano"
   subnet_id     = aws_subnet.public[0].id
   vpc_security_group_ids = [aws_security_group.bastion.id]
