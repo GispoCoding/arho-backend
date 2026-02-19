92727d9a017c67a013088311dcad0a2d2885ec9a
Lauri Kajan <lauri.kajan@gispo.fi>
2026-02-19 22:20:39 +0200

    Refactor cloud-init config to use cloudinit provider

--- infra/bastion.tf
+++ infra/bastion.tf
@@ -5,6 +5,21 @@ data "aws_ssm_parameter" "amazon_linux" {
   name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-arm64"
 }
 
+data "cloudinit_config" "bastion_config" {
+  gzip          = true  # Compresses data to fit more in the 16KB limit
+  base64_encode = true  # AWS requires base64
+
+  part {
+    content_type = "text/cloud-config"
+    content      = templatefile(
+      "bastion_config/cloud-config.yaml.tftpl",
+      {
+        ec2_user_public_keys    = var.bastion_ec2_user_public_keys,
+      }
+    )
+  }
+}
+
 # Just the smallest arm instance available, for routing traffic to postgres
 resource "aws_instance" "bastion-ec2-instance" {
   ami = data.aws_ssm_parameter.amazon_linux.value
@@ -14,7 +29,7 @@ resource "aws_instance" "bastion-ec2-instance" {
   iam_instance_profile = aws_iam_instance_profile.ec2-iam-profile.name
   tenancy              = "default"
   user_data_replace_on_change = true  # This is needed to update user data *and* ip address
-  user_data     = local.bastion_user_data
+  user_data_base64 = data.cloudinit_config.bastion_config.rendered
   tags = merge(local.default_tags, {
     Name = "${var.prefix}-bastion"
   })
